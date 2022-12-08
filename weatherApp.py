7ff3a208304531567ac591f44c984238ef4f0a92
sakshichavre20 <chavresakshi@gmail.com>
2022-12-08 15:09:32 +0530

    Weather App

---
+++ weatherApp.py
@@ -0,0 +1,26 @@
+import json
+import pandas as pd
+import requests
+
+city = input("Enter City Name ")
+date = input("Enter Date yyyy-mm-dd ")
+URL = 'https://api.oikolab.com/weather'
+API_KEY = '658443fc024c4031a1b676c5accdcb17'
+resp = requests.get(URL,
+                 params={'param': ['temperature',],
+
+                         'start': date,
+                         'location' : city,
+                         'end': date,
+                         'freq': 'D',
+                         'api-key': API_KEY}
+                 )
+weather_data = json.loads(resp.json()['data'])
+df = pd.DataFrame(index=pd.to_datetime(weather_data['index'],
+                                       unit='s'),
+                  data=weather_data['data'],
+                  columns=weather_data['columns'])
+
+#print(weather_data)
+temp = df.iloc[0,4]
+print(f"Tempertature for {city} on {date} = {temp}C")
